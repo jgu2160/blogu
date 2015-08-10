a75a6ccb1d3826644391f301cc167c883bb6d08a
jgu <jeffwgu@gmail.com>
2015-08-09 21:06:44 -0600

    loading icon"

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    beforeModel: function() {
+        Ember.$('.loading').removeClass('hidden');
+    },
     model: function() {
         return this.store.find('post');
+    },
+    afterModel: function() {
+        Ember.$('.loading').addClass('hidden');
     }
 });
